c52a87a6e0d2165a0d15702d741821beedeabb4c
Janderson Siqueira <janderssampaio@gmail.com>
2024-10-26 23:13:23 -0300

    Alterações no backend

--- osapi/app.py
+++ osapi/app.py
@@ -1,8 +1,8 @@
-# app.py
 from flask import Flask
 from .routes.mesas_routes import mesas_bp
 from .routes.produtos_routes import produtos_bp
 from .routes.categorias_routes import categorias_bp
+from .routes.pedido_parcial_routes import pedidos_bp
 
 app = Flask(__name__)
 
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.register_blueprint(mesas_bp)
 app.register_blueprint(produtos_bp)
 app.register_blueprint(categorias_bp)
+app.register_blueprint(pedidos_bp)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
