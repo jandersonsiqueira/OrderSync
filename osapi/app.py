48017b25e95a100dc5d2f2fcfc128125c62d48f6
Janderson Siqueira <janderssampaio@gmail.com>
2024-11-26 09:11:20 -0300

    Subindo na vercel

--- osapi/app.py
+++ osapi/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from .routes.mesas_routes import mesas_bp
 from .routes.produtos_routes import produtos_bp
 from .routes.categorias_routes import categorias_bp
@@ -15,6 +15,15 @@ app.register_blueprint(categorias_bp)
 app.register_blueprint(pedido_parcial_bp)
 app.register_blueprint(pedido_final_bp)
 app.register_blueprint(admin_bp)
+
+# Rota para verificar o status do serviço
+@app.route('/about', methods=['GET'])
+def about():
+    return jsonify({
+        "status": "active",
+        "service": "Sistema de Gerenciamento para Restaurantes e Bares",
+        "version": "1.0.0"
+    }), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
